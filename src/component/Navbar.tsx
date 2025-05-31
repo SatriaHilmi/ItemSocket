2d32f045fef50895855a8eda6709bee5eaaf10d0
SatriaHilmi <vanpers34@gmail.com>
2025-05-31 09:08:43 +0700

    fix: pagination for button

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
             <nav className="sticky top-0 left-0 right-0 flex justify-between items-center px-8 py-4 bg-green-500 z-50">
                 <h2 className="text-end font-bold z-20 text-3xl">Biograph</h2>
                 <div className="flex justify-items-center space-x-4">
-                    <img src="/logo192.png" alt="logo" className="w-10 h-10 rounded-full" />
+                    <img src="/CatDev.jpg" alt="logo" className="w-12 h-12 rounded-full" />
                     <ul className="flex gap-4 items-start rounded-2xl bg-green-600 px-4 py-2 shadow-lg">
                         <li onClick={() => setPage('profile')} className="text-black hover:text-white cursor-pointer">Profile</li>
                         <li onClick={() => setPage('home')} className="text-black hover:text-white cursor-pointer">Home</li>
--- src/component/pages/HomePage.tsx
+++ src/component/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import { PopUpOnLoad } from "../PopUpOnLoad";
 import { TextIn } from "../TextIn";
+import { usePage } from "../../hooks/UsePage";
 
 export const HomePage = () => {
+    const { setPage } = usePage();
     return (
         <>
             <div className="flex items-center justify-center h-screen w-full bg-gradient-to-b from-green-500 to-white">
@@ -10,8 +12,8 @@ export const HomePage = () => {
                     <TextIn text="Hello, I'm Satria" />
                     {/* <h1 className="text-6xl font-bold text-gray-800 mb-4">Hello, I'm Satria</h1> */}
                     <p className="text-xl text-gray-600 mb-8">This is my personal web profile.</p>
-                    <button className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300">
-                        About Me
+                    <button className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300" onClick={() => setPage('project')}>
+                        Project
                     </button>
                 </div>
             </div>
